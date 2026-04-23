d4257db23e1f2970e19e6483374b4aed40558615
Danny Dickson <dannydickson093@gmail.com>
2026-04-23 22:40:45 +0300

    Assignment 0: Java Profile Card

---
+++ src/ProfileCard.java
@@ -0,0 +1,28 @@
+public class ProfileCard {
+    public static void main(String[] args) {
+        // Declare variables
+        String name = "Danny Dickson"; // My name
+        String studentreg_no = "2024-04-09556"; // My reg_no
+        String yearOfStudy = "Year 2"; // year of study
+        String programmingBackground = "Eloquent in C, learnt to develop websites using HTML, beginner in Java"; // programming background
+        String courseGoal = "To improve problem-solving skills and enjoy the course alongside"; // course goal
+        String funFact = "I love hiking"; // A fun fact about you
+
+        // Print profile card with border
+        System.out.println("====================================");
+        System.out.println("           CS 234 — JAVA PROFILE CARD       ");
+        System.out.println("====================================");
+        System.out.println("Name: " + name);
+        System.out.println("studentreg_no: " + studentreg_no);
+        System.out.println("Year of Study: " + yearOfStudy);
+        System.out.println("-----------------------------------");
+        System.out.println("Programming Background: " + programmingBackground);
+        System.out.println("-----------------------------------");
+        System.out.println("Course Goal: " + courseGoal);
+        System.out.println("-----------------------------------");
+        System.out.println("Fun Fact: " + funFact);
+        System.out.println("====================================");
+    }
+}
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
