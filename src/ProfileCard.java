6a96de42f24693e51bbad4ec7b4491dd90268263
Danny <your-email@example.com>
2026-04-23 23:25:59 +0300

    Update ProfileCard.java with header comment

--- src/ProfileCard.java
+++ src/ProfileCard.java
@@ -1,15 +1,15 @@
 public class ProfileCard {
     // Assignment 0 - ProfileCard
     public static void main(String[] args) {
-        // Declare variables
+        // Declaring variables
         String name = "Danny Dickson"; // My name
         String studentreg_no = "2024-04-09556"; // My reg_no
         String yearOfStudy = "Year 2"; // year of study
         String programmingBackground = "Eloquent in C, learnt to develop websites using HTML, beginner in Java"; // programming background
         String courseGoal = "To improve problem-solving skills and enjoy the course alongside"; // course goal
-        String funFact = "I love hiking"; // A fun fact about you
+        String funFact = "I am a movie addict "; // A fun fact about me
 
-        // Print profile card with border
+        // Printing profile card with border
         System.out.println("====================================");
         System.out.println("           CS 234 — JAVA PROFILE CARD       ");
         System.out.println("====================================");
