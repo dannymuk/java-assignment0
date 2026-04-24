86fb2aef6bc8330d407d42414089037487388b99
Danny <dannydickson093@gmail.com>
2026-04-24 11:24:56 +0300

    Update ProfileCard.java with header comment

--- src/ProfileCard.java
+++ src/ProfileCard.java
@@ -6,7 +6,7 @@ public class ProfileCard {
         String studentreg_no = "2024-04-09556"; // My reg_no
         String yearOfStudy = "Year 2"; // year of study
         String programmingBackground = "Eloquent in C, learnt to develop websites using HTML, beginner in Java"; // programming background
-        String courseGoal = "To improve problem-solving skills and enjoy the course alongside"; // course goal
+        String courseGoal = "To improve my programmimg in java skills and enjoy the course alongside"; // course goal
         String funFact = "I am a movie addict "; // A fun fact about me
 
         // Printing profile card with border
